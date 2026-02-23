febbf3fd8a39754f366b985bd871675049fb6fde
Friedrich Hoa-Binh Phan <hoa-binh.phan@uni-essen.de>
2026-02-24 01:44:51 +0700

    Initial Commit

---
+++ FabCoachPro/FabCoachProApp.swift
@@ -0,0 +1,17 @@
+//
+//  FabCoachProApp.swift
+//  FabCoachPro
+//
+//  Created by Hoa-Binh Phan on 24.02.26.
+//
+
+import SwiftUI
+
+@main
+struct FabCoachProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
